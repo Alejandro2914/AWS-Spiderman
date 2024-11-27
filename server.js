87e4b4498a8b289b02f38ccb495ffb2b138345ad
Alejandro2914 <alegalindo24@hotmail.com>
2024-11-27 02:58:57 -0500

    feat: :zap: nooow

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
 });
 
 // Iniciar servidor
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Host running in http://localhost:${PORT}`);
 });
